31ea42da528f6007c53c502e5a7fee2360cd886c
readul-islam <developer.readul@gmail.com>
2022-12-05 12:04:28 +0600

    readul

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const departmentsRoute = require('./departments.routes');
 
 const doctorsRoute = require('./doctors.routes');
 const citiesRoutes = require('./cities.routes');
+const patientsRoute = require('./patients.routes');
 
 const hospitalRoute = require('./hospitals.routes');
 
@@ -16,7 +17,7 @@ const appRouter = express.Router()
 appRouter.use(hospitalRoute);
 appRouter.use(statesRoute);
 appRouter.use(citiesRoutes);
-// appRouter.use(patientsRoute);
+appRouter.use(patientsRoute);
 appRouter.use(doctorsRoute);
 appRouter.use(departmentsRoute);
 appRouter.use(eventsRoute);
