a1211c3ac12a8feaa71951b9018a2021ef8b0c72
readul-islam <developer.readul@gmail.com>
2022-12-07 16:31:57 +0600

    r

--- controller/timeSlots.controller.js
+++ controller/timeSlots.controller.js
@@ -24,7 +24,7 @@ exports.getTimeSlots = async (req, res) => {
       where: { date, doctorId },
     });
 console.log(isAvailable)
-    if (!isAvailable) {
+   
     //    const updateValue = {};
     //    updateValue[day] = "9.00am-5.00pm";
       const availability = await WeeklyAvailability.findOne({
@@ -32,16 +32,16 @@ console.log(isAvailable)
       });
       console.log(availability)
      
-      const timeRange=(availability.dataValues[properDay])
+    //   const timeRange=(availability.dataValues[properDay])
       
-      const interval = 15
+    //   const interval = 15
      
-      console.log(timeRange)
-      const [startTime, endTime] = availability[day]?.split('-');
-      console.log(startTime, endTime)
-      const slots =  useTimeSlots('9.00', '12.00', interval)
-      console.log(slots,'slots')
-    res.send({data:true})
+    //   console.log(timeRange)
+    //   const [startTime, endTime] = availability[day]?.split('-');
+    //   console.log(startTime, endTime)
+    //   const slots =  useTimeSlots('9.00', '12.00', interval)
+    //   console.log(slots,'slots')
+    // res.send({data:true})
        
         // const r = await TimeSlots.create({
         //     slots:slots.toString(),
@@ -54,8 +54,8 @@ console.log(isAvailable)
         // }else{
         //     res.status(200).json(time_slots)
         // }
-      
-    } 
+      res.status(200).send({data:true})
+     
   } catch (error) {
     res.status(500).json({ type: error.name, massage: error.massage });
   }
