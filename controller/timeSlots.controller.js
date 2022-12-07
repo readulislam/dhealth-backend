31785c97d29a9a86208e2d2a6dd70bd9b82b987f
readul-islam <developer.readul@gmail.com>
2022-12-07 14:48:41 +0600

    r

--- controller/timeSlots.controller.js
+++ controller/timeSlots.controller.js
@@ -25,21 +25,21 @@ exports.getTimeSlots = async (req, res) => {
     });
 
     if (!isAvailable) {
-      //  const updateValue = {};
-      //  updateValue[day] = "9.00am-5.00pm";
+    //    const updateValue = {};
+    //    updateValue[day] = "9.00am-5.00pm";
       const availability = await WeeklyAvailability.findOne({
         where: { doctorId },
       });
       console.log(day)
-      console.log(availability.dataValues[properDay])
+      const timeRange=(availability.dataValues[properDay])
       console.log('1')
-      if (!availability[day]==='Unavailable') {
+      if (!availability[properDay]==='Unavailable') {
         console.log('1')
         const interval = 15
-        const timeRange= availability[day]
+       
         console.log(timeRange)
         const [startTime, endTime] = availability[day]?.split('-');
-        const slots = await useTimeSlots('9.00', '12.00', interval)
+        const slots = await useTimeSlots(startTime, endTime, interval)
         console.log(slots,'slots')
         const r = await TimeSlots.create({
             slots:{'slots':`${slots}`},
