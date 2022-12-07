48d453dcd60c84df76c8b790f8bd2e1afbbe6a31
readul-islam <developer.readul@gmail.com>
2022-12-07 17:19:25 +0600

    r

--- controller/timeSlots.controller.js
+++ controller/timeSlots.controller.js
@@ -1,5 +1,5 @@
-const { DateOverride, WeeklyAvailability,TimeSlots } = require("../database");
-const moment = require('moment');
+const { DateOverride, WeeklyAvailability, TimeSlots } = require("../database");
+const moment = require("moment");
 const useTimeSlots = require("../hooks/useTimeSlots");
 
 exports.getTimeSlots = async (req, res) => {
@@ -8,88 +8,101 @@ exports.getTimeSlots = async (req, res) => {
   const dateParts = date.split("/");
   const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
   const splitting = dateObject.toString()?.split(" ");
-  const day = (splitting[0]).toLowerCase();
-    let properDay = ''
+  const day = splitting[0].toLowerCase();
+  let properDay = "";
   switch (day) {
-    case 'tue':
-        properDay='tuesday'
+    case "mon":
+      properDay = "monday";
+      break;
+
+    case "tue":
+      properDay = "tuesday";
+      break;
+      case "wed":
+        properDay = "wednesday";
         break;
-  
+    case "thur":
+      properDay = "thursday";
+      break;
+    case "fri":
+      properDay = "friday";
+      break;
+    case "sat":
+      properDay = "saturday";
+      break;
+    case "sun":
+      properDay = "sunday";
+      break;
+
     default:
-        break;
+      break;
   }
 
   try {
     const isAvailable = await DateOverride.findOne({
       where: { date, doctorId },
     });
-console.log(isAvailable)
-   
-    //    const updateValue = {};
-    //    updateValue[day] = "9.00am-5.00pm";
-      const availability = await WeeklyAvailability.findOne({
-        where: { doctorId },
-      });
-      console.log(availability)
-     
-      const timeRange=(availability.dataValues[properDay])
-      
-    //   const interval = 15
-     
-    //   console.log(timeRange)
-    //   const [startTime, endTime] = availability[day]?.split('-');
-    //   console.log(startTime, endTime)
-    const useTimeSlots=(start, end)=>{
-      var startTime = moment(start, 'HH:mm');
-      var endTime = moment(end, 'HH:mm');
-      
-      if( endTime.isBefore(startTime) ){
-        endTime.add(1, 'day');
-      }
-    
-      var timeStops = [];
-     var id = 1
-      while(startTime <= endTime){
-          const timeObj={
-              id,
-              time:new moment(startTime).format('HH:mm'),
-              isAvailable:false,
-  
-          }
-      timeStops.push(timeObj);
-        startTime.add(15, 'minutes');
-        id++;
-      }
-      return timeStops;
-    }
-    const s = useTimeSlots('9.00','17.00')
-    console.log(s)
-    // res.send({data:true})
-       const find = await TimeSlots.findOne({where:{weekday:properDay, timeRange}})
-       console.log(find)
-       if(find){
-        res.status(200).json(find)
-       }else{
-        const r = await TimeSlots.create({
-          slots:slots.toString(),
-          timeRange,
-          weekday:properDay
+ if(!isAvailable)  {
+  const availability = await WeeklyAvailability.findOne({
+    where: { doctorId },
+  });
+  const timeRange = availability.dataValues[properDay];
+  if(timeRange === 'Unavailable'){
+    res.status(200).json({data:'Unavailable'})
+    return;
+  }
+  const [startTime, endTime] = timeRange?.split('-');
+ 
 
-      })
-      res.status(200).json({data:r})
-       }
-        
+
   
-        // if(created) {
-        //     res.status(200).json(created)
-        // }else{
-        //     res.status(200).json(time_slots)
-        // }
-     
-     
+  
+  const useTimeSlots = (start, end) => {
+    var startTime = moment(start, "HH:mm");
+    var endTime = moment(end, "HH:mm");
+
+    if (endTime.isBefore(startTime)) {
+      endTime.add(1, "day");
+    }
+
+    var timeStops = [];
+    var id = 1;
+    while (startTime <= endTime) {
+      const timeObj = {
+        id,
+        time: new moment(startTime).format("HH:mm"),
+        isAvailable: false,
+      };
+      timeStops.push(timeObj);
+      startTime.add(15, "minutes");
+      id++;
+    }
+    return timeStops;
+  };
+  const s = useTimeSlots(startTime, endTime);
+  
+  const find = await TimeSlots.findOne({
+    where: { weekday: properDay, timeRange },
+  });
+  console.log(find);
+  if (find) {
+    res.status(200).json(find);
+  } else {
+    const r = await TimeSlots.create({
+      slots: slots.toString(),
+      timeRange,
+      weekday: properDay,
+    });
+    res.status(200).json({ data: r });
+  }
+
+ }else{
+  res.status(200).json({data:'Unavailable'})
+ }
+
+   
+    
   } catch (error) {
     res.status(500).json({ type: error.name, massage: error.massage });
   }
-
-
 };
