0230bcce825cb681c83d989c1753dcaf8913f8c3
readul-islam <developer.readul@gmail.com>
2022-12-07 14:22:23 +0600

    r

--- controller/timeSlots.controller.js
+++ controller/timeSlots.controller.js
@@ -9,39 +9,41 @@ exports.getTimeSlots = async (req, res) => {
   const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
   const splitting = dateObject.toString()?.split(" ");
   const day = (splitting[0] + "day").toLowerCase();
-  try {
-    const isAvailable = await DateOverride.findOne({
-      where: { date, doctorId },
-    });
+//   try {
+//     const isAvailable = await DateOverride.findOne({
+//       where: { date, doctorId },
+//     });
 
-    if (!isAvailable) {
-      //  const updateValue = {};
-      //  updateValue[day] = "9.00am-5.00pm";
-      const availability = await WeeklyAvailability.findOne({
-        where: { doctorId },
-      });
-      if (!availability[day]==='Unavailable') {
-        const interval = 15
-        const timeRange= availability[day]
-        const [startTime, endTime] = availability[day]?.split('-');
-        const slots = await useTimeSlots(startTime, endTime, interval)
-        console.log(slots,'slots')
-        const r = await TimeSlots.create({
-            slots:{'slots':`${slots}`},
-            timeRange,
-            weekday:day
-        })
-res.send(r)
-        // if(created) {
-        //     res.status(200).json(created)
-        // }else{
-        //     res.status(200).json(time_slots)
-        // }
-      }
-    } else {
-      res.status(200).json({ data: "Unavailable" });
-    }
-  } catch (error) {
-    res.status(500).json({ type: error.name, massage: error.massage });
-  }
+//     if (!isAvailable) {
+//       //  const updateValue = {};
+//       //  updateValue[day] = "9.00am-5.00pm";
+//       const availability = await WeeklyAvailability.findOne({
+//         where: { doctorId },
+//       });
+//       if (!availability[day]==='Unavailable') {
+//         const interval = 15
+//         const timeRange= availability[day]
+//         const [startTime, endTime] = availability[day]?.split('-');
+//         const slots = await useTimeSlots(startTime, endTime, interval)
+//         console.log(slots,'slots')
+//         const r = await TimeSlots.create({
+//             slots:{'slots':`${slots}`},
+//             timeRange,
+//             weekday:day
+//         })
+// res.send(r)
+//         // if(created) {
+//         //     res.status(200).json(created)
+//         // }else{
+//         //     res.status(200).json(time_slots)
+//         // }
+//       }
+//     } else {
+//       res.status(200).json({ data: "Unavailable" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ type: error.name, massage: error.massage });
+//   }
+
+res.send({success:true})
 };
