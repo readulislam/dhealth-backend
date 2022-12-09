821a1500de4cc7eed2fc41e4f11c904e4b28f0e8
readul-islam <developer.readul@gmail.com>
2022-12-09 17:53:53 +0600

    r

--- controller/doctors.controller.js
+++ controller/doctors.controller.js
@@ -1,62 +1,54 @@
-const multer = require('multer')
-const path = require('path')
-const {Doctors,Departments,Hospitals} = require('../database')
-exports.createDoctor = async(req,res) =>{
+const multer = require("multer");
+const path = require("path");
+const { Doctors, Departments, Hospitals } = require("../database");
+exports.createDoctor = async (req, res) => {
+  try {
+    const doctor = await Doctors.create({ ...req.body });
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ type: error.name, massage: error.massage });
+  }
+};
+exports.getDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctors.findAll({
+      include: [
+        { model: Departments, as: "department" },
+        { model: Hospitals, as: "hospital" },
+      ],
+    });
+    res.status(200).json(doctors);
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.dropDoctor = async (req, res) => {
+  const { id } = req.query;
+  try {
+    const dropped = await Doctors.destroy({ where: { id } });
+    res.status(200).json(dropped);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-try {
-    const doctor = await  Doctors.create({...req.body,})
-    res.status(200).json(doctor)
-} catch (error) {
-    res.status(500).json({type:error.name, massage:error.massage})
-}
-}
-exports.getDoctors = async(req,res) =>{
+exports.getDoctorByPhone = async (req, res) => {
+  console.log(req.query);
+  // console.log(typeof req.params.contactNo)
 
-try {
-    const doctors = await  Doctors.findAll({
-        include: [
-            { model: Departments, as: "department" },
-            { model: Hospitals, as: "hospital" },]
-    })
-    res.status(200).json(doctors)
-} catch (error) {
-    console.log(error)
-}
-}
-exports.dropDoctor = async(req,res) =>{
-const {id} = req.query
-try {
-    const dropped = await  Doctors.destroy({where:{id}})
-    res.status(200).json(dropped)
-} catch (error) {
-    console.log(error)
-}
-}
-
-exports.getDoctorByPhone = async(req,res) =>{
-    console.log(req.query)
-    // console.log(typeof req.params.contactNo)
-  
-    try {
-        const doctor = await  Doctors.findOne({where:{contactNo:req.query.contactNo}})
-        res.status(200).json(doctor)
-    } catch (error) {
-        res.status(500).json({type:error.name, massage:error.massage})
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+  try {
+    const doctor = await Doctors.findOne({
+      where: { contactNo: req.query.contactNo },
+      include: [
+        { model: Departments, as: "department" },
+        { model: Hospitals, as: "hospital" },
+      ],
+    });
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ type: error.name, massage: error.massage });
+  }
+};
 
 // upload Image file for doctors
 // const storage = multer.diskStorage({
