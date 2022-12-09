105d2b15451e85b46717e893444a25ac909c2ecc
readul-islam <developer.readul@gmail.com>
2022-12-09 16:33:24 +0600

    readul

--- controller/timeSlots.controller.js
+++ controller/timeSlots.controller.js
@@ -120,7 +120,11 @@ exports.dropSlots = async (req, res) => {
 };
 
 exports.updateSlot = async (req, res) => {
-  const { doctorId, date, timeRange, slotId } = req.query;
+  // const { doctorId, date, timeRange, slotId } = req.query;
+  let doctorId = 1;
+  let date = '08/12/2022';
+  let timeRange ='9.00-17.00';
+  let slotId = 1;
   try {
     const findSlots = await TimeSlote.findOne({
       where: { doctorId, date, timeRange,weekday },
