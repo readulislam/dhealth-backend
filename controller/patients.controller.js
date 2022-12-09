acec4a03314a1f547d7779f1934cdab5a7c5dd7b
readul-islam <developer.readul@gmail.com>
2022-12-09 20:06:48 +0600

    r

--- controller/patients.controller.js
+++ controller/patients.controller.js
@@ -7,3 +7,17 @@ exports.patientRegistration = async(req,res) =>{
         res.status(500).json({type:error.name, massage:error.massage});
     }
 }
+exports.getPatientByPhone = async(req,res) =>{
+    const {contact} = req.query
+    try {
+        const patient = await Patients.findOne({where:{contact}});
+        if(patient){
+            res.status(200).json(patient);
+        }else{
+            res.status(200).json({massage:"You arn't  registered user"})
+        }
+       
+    } catch (error) {
+        res.status(500).json({type:error.name, massage:error.massage});
+    }
+}
--- routes/patients.routes.js
+++ routes/patients.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { patientRegistration } = require('../controller/patients.controller');
+const { patientRegistration, getPatientByPhone } = require('../controller/patients.controller');
 const patientsRoute = express.Router();
 
 patientsRoute.post('/patient-registration', patientRegistration)
+patientsRoute.get('/patient-login', getPatientByPhone)
 
 
 
