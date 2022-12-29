2f5f4101a39bb0a252f0f5427f117679fc5d6ae2
readulislam <readul.futurionic@gmail.comgit config --global user.email readul.futurionic@gmail.com>
2022-12-29 03:22:36 -0500

    update departments

--- controller/hospitals.controller.js
+++ controller/hospitals.controller.js
@@ -3,7 +3,7 @@ const { Hospitals } = require("../database");
 
 // create a new hospital
 exports.createHospital = async (req, res) => {
-  try {bulkCreate
+  try {
     // const hospital = await Hospitals.create(req.body);
     const hospital = await Hospitals.bulkCreate(req.body);
     res.status(200).send({ success: true, data: hospital });
