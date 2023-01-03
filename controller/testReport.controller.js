bcb9b2646e6087fca724bb789e2b1d457d740eff
himanshu-futurionic11 <himanshu.futurionic@gmail.com>
2023-01-03 13:29:10 +0530

    report

--- controller/testReport.controller.js
+++ controller/testReport.controller.js
@@ -5,42 +5,42 @@ const path = require('path')
 exports.createTestReport = async (req, res) => {
     console.log(req.body)
   try {
-    let info = {
-      link: req.file.path,
-      name: req.body.name,
-      doctorId: req.body.doctorId,
-      patientId: req.body.patientId,
-      appointmentId: req.body.appointmentId,
-  }
-    const addTestReport = await TestReports.create(info);
+  //   let info = {
+  //     link: req.file.path,
+  //     name: req.body.name,
+  //     doctorId: req.body.doctorId,
+  //     patientId: req.body.patientId,
+  //     appointmentId: req.body.appointmentId,
+  // }
+    const addTestReport = await TestReports.create(req.body);
     res.status(200).json(addTestReport)
   } catch (error) {
     res.status(500).json({type:error.name, massage:error.massage})
   }
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'uploadedFile/reports')
-  },
-  filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname))
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//       cb(null, 'uploadedFile/reports')
+//   },
+//   filename: (req, file, cb) => {
+//       cb(null, Date.now() + path.extname(file.originalname))
+//   }
+// })
 
-exports.uploadReport = multer({
-  storage: storage,
-  limits: { fileSize: '1000000' },
-  // fileFilter: (req, file, cb) => {
-  //     const fileTypes = /pdf|xlsx/
-  //     const mimeType = fileTypes.test(file.mimetype)
-  //     const extname = fileTypes.test(path.extname(file.originalname))
-  //         if(mimeType && extname) {
-      //             return cb(null, true)
-      //         }
-      //         cb('Give proper files formate to upload')
-  // }
-}).single('file')
+// exports.uploadReport = multer({
+//   storage: storage,
+//   limits: { fileSize: '1000000' },
+//   // fileFilter: (req, file, cb) => {
+//   //     const fileTypes = /pdf|xlsx/
+//   //     const mimeType = fileTypes.test(file.mimetype)
+//   //     const extname = fileTypes.test(path.extname(file.originalname))
+//   //         if(mimeType && extname) {
+//       //             return cb(null, true)
+//       //         }
+//       //         cb('Give proper files formate to upload')
+//   // }
+// }).single('file')
 
 exports.getTestReport = async (req, res) => {
     console.log(req.body)
