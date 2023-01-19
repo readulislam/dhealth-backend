b48e3f08ff6ec5bf95344571327111e873660b7b
himanshu-futurionic11 <himanshu.futurionic@gmail.com>
2023-01-19 16:33:53 +0530

    disease

--- controller/disease.controller.js
+++ controller/disease.controller.js
@@ -9,7 +9,7 @@ exports.createDisease = async(req,res) =>{
     }
 }
 exports.getDisease = async(req,res) =>{
-    const {departmentId} = req.body;
+    const {departmentId} = req.query;
     try {
     const getDisease = await   Disease.findAll({where:{departmentId}}) ;
     res.status(200).json(getDisease) 
